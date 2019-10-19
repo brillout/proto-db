712a95a64ed9e818ce6e4bec35d0a7c2b8c34e63
Romuald Brillout <github.com@brillout.com>
2019-10-19 19:00:45 +0200

    improve interface

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ module.exports = {
   getUUID,
 };
 
-function load(databaseFile, defaultValue={}) {
+function load(databaseFile, defaultValue) {
   assert.usage(path.isAbsolute(databaseFile), "`databaseFile` should be an absolute path.", {databaseFile});
 
   let protoObj;
@@ -15,6 +15,7 @@ function load(databaseFile, defaultValue={}) {
     protoObj = fs.readJsonSync(databaseFile);
   } catch(err) {
     assert.usage(!fs.pathExistsSync(databaseFile), "`"+databaseFile+"` should be a JSON file.");
+    assert.usage(defaultValue!==undefined, "You should provide a `defaultValue` when there is no `"+databaseFile+"` file.");
     protoObj = defaultValue;
   }
 
